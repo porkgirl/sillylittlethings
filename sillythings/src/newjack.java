30623c184b50cf0325f76f446c93fe14e8a8cb53
Emily Murray <emilym28@swrobotics.com>
2025-10-12 15:34:23 -0500

    Updating Newjack

--- sillythings/src/newjack.java
+++ sillythings/src/newjack.java
@@ -11,11 +11,9 @@ static List<Integer> dhand = new ArrayList<>();
         deck();
         deal('p');
         deal('d');
-        System.out.println(phand);
-        System.out.println(dhand);
+        compile('a');
     }
     static void deck(){
-        List<Integer> deck = new ArrayList<>();
         for (int i = 0; i < 4; i++){
             for (int n = 1; n <  10; n++){
                 deck.add(n);
@@ -24,7 +22,7 @@ static List<Integer> dhand = new ArrayList<>();
                 deck.add(10);
             }
             Collections.shuffle(deck);
-            System.out.println(deck);
+            //System.out.println(deck);
         }
     }
     static void deal(Character hand){
@@ -35,5 +33,28 @@ static List<Integer> dhand = new ArrayList<>();
                 dhand.add(deck.remove(0));
             }
         }
+    }
+    static void compile(Character sum){
+        int psum = 0;
+        int dsum = 0;
+
+        if (sum == 'a'){
+            for (int c = 0; c < dhand.size(); c++){
+                dsum += dhand.get(c);
+            }
+            for (int c = 0; c < phand.size(); c++){
+                psum += phand.get(c);
+            }
+        }else if (sum == 'p'){
+            for (int c = 0; c < phand.size(); c++){
+                psum += phand.get(c);
+            }
+        }else if (sum == 'd'){
+            for (int c = 0; c < dhand.size(); c++){
+                dsum += dhand.get(c);
+            }
+        }
+        System.out.println(psum + " | " + dsum);
+
     }
 }
