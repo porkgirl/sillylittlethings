8775663b7f6f3c32bb050002b19432871a6075cb
Emily Murray <emilym28@swrobotics.com>
2025-10-31 16:56:48 -0500

    hi hi hi hi  :3

--- sillythings/src/hexcode.java
+++ sillythings/src/hexcode.java
@@ -4,15 +4,26 @@ import java.util.Collections;
 import java.util.List;
 public class hexcode{
     public static void main(String[] args){
-        List<Character> alpha = new ArrayList<>();
-        Collections.addAll(alpha,'a','b','c','d','e','f');
+        String digit = "0";
         Random numeric = new Random();
-        int min = 1;
-        int max = 7;
-        int num = numeric.nextInt(min - max + 1);
-        for (int y = 0; y < 13; y++){
-            System.out.println(num);
-        }
-        System.out.println();
+        List<String> alpha = new ArrayList<>();
+        Collections.addAll(alpha,"a", "b", "c", "d", "e", "f");
+        List<String> code = new ArrayList<>();
+        for (int i = 0; i < 6; i++){    
+            int type = numeric.nextInt(0,2);
+            if (type == 1){
+                int numerals = numeric.nextInt(0, 10);
+                System.out.println(numerals);
+            }else if (type == 0){
+                //letters
+                int num = numeric.nextInt(0, 6);
+                System.out.println(alpha.get(num));
+                digit = alpha.get(num);
+            }
+                //code
+                
+                code.add(digit);
+        }   
+        System.out.println(code);
     }
 }
