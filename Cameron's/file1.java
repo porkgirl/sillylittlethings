22b28db55c6883bd7040cf84dfb2f013bfa054a7
Emily Murray <emilym28@swrobotics.com>
2026-02-02 17:27:08 -0600

    Update file1.java

--- Cameron's/file1.java
+++ Cameron's/file1.java
@@ -1,13 +1,22 @@
+import java.util.concurrent.TimeUnit;
+
 public class file1 {
 
     public static file2 file2;
 
-
     public static void main(String[] args) {
         file2 = new file2();
         
+        while(true) {
+
         file2.Deck();
 
         file2.Combine();
+
+        try { TimeUnit.SECONDS.sleep(1); } 
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+              }
+        }
     }
 }
